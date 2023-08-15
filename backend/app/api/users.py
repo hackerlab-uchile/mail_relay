ecdce5ed4946784e06bade127af796d5e5d653b9
svaldivia1996 <sebastianvaldiviareyes@hotmail.com>
2023-08-15 16:47:09 -0400

    fix: User object is created after verifing that the username is available

--- backend/app/api/users.py
+++ backend/app/api/users.py
@@ -11,9 +11,9 @@ router = APIRouter()
 
 @router.post("/")
 def create_user(user: UserBase, db: Session = Depends(get_db)):
-    db_user = User(username=user.username, password=user.password, remember_token=user.remember_token, recipient_email=user.recipient_email)
     if crud_users.get_user_by_username(db, username=user.username):
         raise HTTPException(status_code=400, detail="Username already registered")
+    db_user = User(username=user.username, password=user.password, remember_token=user.remember_token, recipient_email=user.recipient_email)
     return crud_users.create_user(db=db, user=db_user)
 
 @router.get("/all/")
