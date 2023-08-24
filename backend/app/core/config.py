8bd6c2d02958e2a8a670bf5218f19eb0473e1124
svaldivia1996 <sebastianvaldiviareyes@hotmail.com>
2023-08-24 14:05:22 -0400

    fix: eliminated default env values on backend config and updated .env template

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,6 +1,6 @@
 import os
 
-DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql+psycopg2://postgres:postgres@db/postgres")
-SECRET_KEY = os.environ.get("SECRET_KEY","09d25e094faa6ca2556c818166b7a3182b93f7099f6f0f4caz6cf63b88e8d3e7") # openssl rand -hex 32
-ALGORITHM = os.environ.get("ALGORITHM","HS256")
-ACCESS_TOKEN_EXPIRE_MINUTES = os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES", 20)
+DATABASE_URL = os.environ.get("DATABASE_URL")
+SECRET_KEY = os.environ.get("SECRET_KEY") # openssl rand -hex 32
+ALGORITHM = os.environ.get("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES")
