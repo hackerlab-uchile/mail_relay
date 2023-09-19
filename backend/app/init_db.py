129aea7c7a757e8a8e81ed931f19f9c41356c233
svaldivia1996 <sebastianvaldiviareyes@hotmail.com>
2023-09-19 15:04:46 -0300

    refactor: removed remember token on User model & Webauthkeys

--- backend/app/init_db.py
+++ backend/app/init_db.py
@@ -1,7 +1,7 @@
 from app.core.database import engine, Base, test_engine
 from app.models.users import User
 from app.models.aliases import Alias
-from app.models.webauth_keys import WebauthKey
+#from app.models.webauth_keys import WebauthKey
 
 print("Creating database tables...")
 Base.metadata.create_all(bind=engine)
--- backend/app/models/users.py
+++ backend/app/models/users.py
@@ -9,7 +9,6 @@ class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     username: Mapped[str] = mapped_column(unique=True, index=True)
     password: Mapped[str] = mapped_column(String(255))
-    remember_token: Mapped[str] = mapped_column(String(100))
     recipient_email: Mapped[str] = mapped_column(String(255), unique=True, index=True)
 
 #PYDANTIC
@@ -19,4 +18,3 @@ class UserBase(BaseModel):
 
 class UserCreate(UserBase):
     password: str
-    remember_token: str
