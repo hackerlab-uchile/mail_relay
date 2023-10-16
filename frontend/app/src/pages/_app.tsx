540b9aadf60f24beaec095992656fffea645cf49
svaldivia1996 <sebastianvaldiviareyes@hotmail.com>
2023-10-16 16:27:57 -0300

    feat: added snackbars

--- frontend/app/src/pages/_app.tsx
+++ frontend/app/src/pages/_app.tsx
@@ -2,14 +2,17 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { SnackbarProvider } from "notistack";
 
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <SnackbarProvider>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </SnackbarProvider>
   );
 }
--- frontend/app/src/utils/axios.ts
+++ frontend/app/src/utils/axios.ts
@@ -2,9 +2,9 @@ import axios, { HeadersDefaults } from "axios";
 
 let BACKEND_HOST = process.env.NEXT_PUBLIC_BACKEND_HOST || "http://localhost";
 let BACKEND_PORT = process.env.NEXT_PUBLIC_BACKEND_PORT || "8000";
-let PROD = process.env.NEXT_PUBLIC_PROD || false;
+let PROD = process.env.NEXT_PUBLIC_PROD;
 
-let base_url = PROD ? BACKEND_HOST : `${BACKEND_HOST}:${BACKEND_PORT}`;
+let base_url = PROD=="true" ? BACKEND_HOST : `${BACKEND_HOST}:${BACKEND_PORT}`;
 console.log("Production mode", PROD);
 console.log("backend:base_url", base_url);
 
