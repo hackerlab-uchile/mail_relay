52c85001ca6316786599988216f9dbb260b7fa6d
svaldivia1996 <sebastianvaldiviareyes@hotmail.com>
2023-10-30 17:33:45 -0300

    feat:  Env templating recipients.cf and virtual.cf

--- postfix/init-scripts/install-pg-client.sh
+++ postfix/init-scripts/install-pg-client.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
-apk add postfix-pgsql  # alpine
+#apk add postfix-lmdb   # alpine
+apk add envsubst
+apk add postfix-pgsql
+
+# Add env variables to postfix config files
+envsubst < /etc/postfix/recipients.cf.template > /etc/postfix/recipients.cf
+envsubst < /etc/postfix/virtual.cf.template > /etc/postfix/virtual.cf
+
+# Create postfix lookup tables
 postmap /etc/postfix/recipients.cf
 postmap /etc/postfix/virtual.cf
