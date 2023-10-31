3b4dbb9b32f1348f906bf50cc9669f55492d6ba3
svaldivia1996 <sebastianvaldiviareyes@hotmail.com>
2023-10-31 15:07:57 -0300

    feat: submissions and local postfix services block

--- postfix/init-scripts/install-pg-client.sh
+++ postfix/init-scripts/install-pg-client.sh
@@ -10,3 +10,35 @@ envsubst < /etc/postfix/virtual.cf.template > /etc/postfix/virtual.cf
 # Create postfix lookup tables
 postmap /etc/postfix/recipients.cf
 postmap /etc/postfix/virtual.cf
+
+# Modify master.cf
+cat <<EOF > /etc/postfix/master.cf
+smtp      inet  n       -       n       -       -       smtpd
+submission inet n       -       n       -       -       smtpd
+#submissions     inet  n       -       n       -       -       smtpd
+pickup    unix  n       -       n       60      1       pickup
+cleanup   unix  n       -       n       -       0       cleanup
+qmgr      unix  n       -       n       300     1       qmgr
+tlsmgr    unix  -       -       n       1000?   1       tlsmgr
+rewrite   unix  -       -       n       -       -       trivial-rewrite
+bounce    unix  -       -       n       -       0       bounce
+defer     unix  -       -       n       -       0       bounce
+trace     unix  -       -       n       -       0       bounce
+verify    unix  -       -       n       -       1       verify
+flush     unix  n       -       n       1000?   0       flush
+proxymap  unix  -       -       n       -       -       proxymap
+proxywrite unix -       -       n       -       1       proxymap
+smtp      unix  -       -       n       -       -       smtp
+relay     unix  -       -       n       -       -       smtp
+        -o syslog_name=postfix/$service_name
+showq     unix  n       -       n       -       -       showq
+error     unix  -       -       n       -       -       error
+retry     unix  -       -       n       -       -       error
+discard   unix  -       -       n       -       -       discard
+#local     unix  -       n       n       -       -       local
+virtual   unix  -       n       n       -       -       virtual
+lmtp      unix  -       -       n       -       -       lmtp
+anvil     unix  -       -       n       -       1       anvil
+scache    unix  -       -       n       -       1       scache
+postlog   unix-dgram n  -       n       -       1       postlogd
+EOF
