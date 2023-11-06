c466706415c05d15cdce0dd365e71995ddb38ab9
svaldivia1996 <sebastianvaldiviareyes@hotmail.com>
2023-11-06 13:06:25 -0300

    feat: Add FailedDelivery model to database

--- backend/app/init_db.py
+++ backend/app/init_db.py
@@ -2,6 +2,7 @@ from app.core.database import engine, Base, test_engine
 from app.models.users import User
 from app.models.aliases import Alias
 from app.models.correct_deliveries import CorrectDelivery
+from app.models.failed_deliveries import FailedDelivery
 
 # from app.models.webauth_keys import WebauthKey
 
---
+++ backend/app/models/failed_deliveries.py
@@ -0,0 +1,17 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Column, Integer, DateTime, String
+from app.core.database import Base
+import datetime
+
+
+class FailedDelivery(Base):
+    __tablename__ = "failed_deliveries"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    timestamp: Mapped[datetime.datetime] = mapped_column(
+        DateTime, default=datetime.datetime.utcnow
+    )
+    from_address: Mapped[str] = mapped_column(String)
+    to_address: Mapped[str] = mapped_column(String)
+    status: Mapped[str] = mapped_column(String)
+    reason: Mapped[str] = mapped_column(String)
