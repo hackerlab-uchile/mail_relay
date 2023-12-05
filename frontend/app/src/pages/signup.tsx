8486fb1493ef11929c52a9d62633fecc90eecedf
svaldivia1996 <sebastianvaldiviareyes@hotmail.com>
2023-12-04 21:12:36 -0300

    chore: change error message in signup page

--- frontend/app/src/pages/signup.tsx
+++ frontend/app/src/pages/signup.tsx
@@ -64,7 +64,7 @@ export default function Signup() {
       try {
         await signupMutation.mutateAsync(user);
       } catch (error) {
-        enqueueSnackbar("Error en la creación de cuenta: " + error, {
+        enqueueSnackbar("Error en la creación de cuenta: ", {
           variant: "error",
           autoHideDuration: 3000,
         });
